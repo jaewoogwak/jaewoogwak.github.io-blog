b96de563212c87d09e9660a26dc132701afa9689
jaewoogwak <jaewoo010207@gmail.com>
2022-05-10 17:02:31 +0900

    [파일명 수정]

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,5 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import Category from "./category"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
